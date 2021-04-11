7aae3d0dd8cc90c8b4d9225a9a4e81320c483d64
bibin <bibinjohny@hotmail.com>
2021-04-11 18:02:15 +0530

    test

--- app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -49,6 +49,7 @@ import org.tensorflow.lite.examples.detection.tracking.MultiBoxTracker;
 public class DetectorActivity extends CameraActivity implements OnImageAvailableListener {
   private static final Logger LOGGER = new Logger();
 
+  //hi
   // Configuration values for the prepackaged SSD model.
   private static final int TF_OD_API_INPUT_SIZE = 320;
   private static final boolean TF_OD_API_IS_QUANTIZED = false;
