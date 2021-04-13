a6e337c9360c2ef92dc611b08b2f167bbe37564a
bibin <bibinjohny@hotmail.com>
2021-04-13 12:25:56 +0530

    changed model, changed input size from 320 to 300.

--- app/src/main/java/org/tensorflow/lite/examples/detection/CameraConnectionFragment.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/CameraConnectionFragment.java
@@ -70,7 +70,7 @@ public class CameraConnectionFragment extends Fragment {
    * The camera preview size will be chosen to be the smallest frame by pixel size capable of
    * containing a DESIRED_SIZE x DESIRED_SIZE square.
    */
-  private static final int MINIMUM_PREVIEW_SIZE = 320;
+  private static final int MINIMUM_PREVIEW_SIZE = 300;
 
   /** Conversion from screen rotation to JPEG orientation. */
   private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
--- app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -51,7 +51,7 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
  //test comment
   //hi
   // Configuration values for the prepackaged SSD model.
-  private static final int TF_OD_API_INPUT_SIZE = 320;
+  private static final int TF_OD_API_INPUT_SIZE = 300;
   private static final boolean TF_OD_API_IS_QUANTIZED = false;
   private static final String TF_OD_API_MODEL_FILE = "notquantizedv3.tflite";
   private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/label43segnali.txt";
